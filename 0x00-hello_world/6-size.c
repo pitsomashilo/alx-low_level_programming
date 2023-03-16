7aa909527a07f447d43e819b1cff6d5a1dbae13e
pitsomashilo <mmapitsomashilo@gmail.com>
2023-03-16 07:30:44 -0700

    Wrote a C program that prints the sizes of various data types + updated the README.md file with the details of what this program does

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints various sizes of data types
+
+ * The function uses the sizeof function and prints the size of the data types
+ * char, int, long int and float
+ * Return: 0
+*/
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -11,3 +11,5 @@ Script 3-name compiles a C file and creates an executable named cisfun
 The program puts.c prints the string \"Programming us like building a multilingual puzzle\" followed by a new line
 
 5-printf.c is a C program that prints exactly \"with proper grammer, but the outcome is a piece of art,\" followed by a newline
+
+6-size.c is a C program that prints the size of various types on the computer it is compiled and run on
