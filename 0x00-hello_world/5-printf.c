4dd31e76afa22b9a1e779e8f0c993e45f5db98c3
pitsomashilo <mmapitsomashilo@gmail.com>
2023-03-16 04:43:41 -0700

    Made changes to 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,8 +6,6 @@
 */
 int main(void)
 {
-	char str = "with proper grammar, but the outcome is a piece of art,";
-
-	printf("%s\n", str);
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
