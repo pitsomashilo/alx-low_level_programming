38057aad93c5ba6bc0326685204ff0df20efcd4c
pitsomashilo <mmapitsomashilo@gmail.com>
2023-03-16 07:38:32 -0700

    Made changes to 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints various sizes of data types
-
+ *
  * The function uses the sizeof function and prints the size of the data types
  * char, int, long int and float
  * Return: 0
@@ -12,6 +12,7 @@ int main(void)
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
