a581661e920c4f1df547a5fb606b2bbeb41511ca
pitsomashilo <mmapitsomashilo@gmail.com>
2023-03-16 09:35:22 -0700

    Made changes to 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,6 +12,6 @@ int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-	fwrite(str, 1, sizeof(str)-1, stderr);
+	fwrite(&str, 1, sizeof(str), stderr);
 	return (1);
 }
