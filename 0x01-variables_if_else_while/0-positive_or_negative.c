ca8360d213c935b09ba6e11eedcbafd2b0652f7b
pitsomashilo <mmapitsomashilo@gmail.com>
2023-03-17 18:10:47 +0000

    Changed int types in 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,15 +18,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n == 0)
 	{
-		printf("%ld is zero\n", n);
+		printf("%d is zero\n", n);
 	}
 	else if (n < 0)
 	{
-		printf("%ld is negative\n", n);
+		printf("%d is negative\n", n);
 	}
 	else
 	{
-		printf("%ld is positive\n");
+		printf("%d is positive\n", n);
 	}
 	return (0);
 }
