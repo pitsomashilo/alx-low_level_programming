0d802ed017e0c85c851f5e926e5f75054687a84c
pitsomashilo <mmapitsomashilo@gmail.com>
2023-03-17 11:36:55 +0000

    Wrote a C program that uses the function putchar to print the alphabet in lowercase followed by a newline + added the details of this program to the README.md file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h.
+
+/**
+ * main - prints the alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	char a = 97;
+	char space = '\n';
+
+	while (a < 123)
+	{
+		putchar(a++);
+	}
+	putchar(space);
+	return (0);
+}
