7b95008c682134b41441931da46e078a2c3c53ea
pitsomashilo <mmapitsomashilo@gmail.com>
2023-03-17 10:06:09 +0000

    Completed the source code for 1-last_digit.c + added details of what the program does to the README.md file

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints a string describing the last digit of the number stored in n
+ *
+ * The program assigns a random number to n every time it is executed, finda
+ * the last digit in the number stored in n and states whether the last digit
+ * is greater than 5, is zero or is less than 6 and not zero.
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int y = 10;
+	int last_dig = n % y;
+	char str[] = "Last digit of";
+
+	if (last_dig != 0 && last_dig < 6)
+	{
+		printf("%s %ld is %d and is less than 6 and not 0\n", str, n, last_dig);
+	}
+	else if (last_dig == 0)
+	{
+		printf("%s %ld is %d and is 0\n", str, n, last_dig);
+	}
+	else if (last_digit > 5)
+	{
+		printf("%s %ld is %d and is greater than 5\n", str, n, last_dig);
+	}
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,5 @@
 Variables, If-Else and While Statements
 
 The program 0-positive_or_negative.c assigns a random number to the variable n each time it is executed, and prints a string that states whether the number stored in n is positive, negative or zero.
+
+The program 1-last_digit.c assigns a random number to the variable n each time it is executed, finds the last digit in the number stored in n and states whether the last digit is greater than 5, is 0 or is less than 6 and not 0.
