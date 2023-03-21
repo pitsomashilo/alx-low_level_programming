f56d1d3810c63158ec811d140101f50f601f3625
pitsomashilo <mmapitsomashilo@gmail.com>
2023-03-21 17:26:32 +0000

    - Added main.h header file and 0-putchar, a C program that prints _putchar to stdout

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * main - prints _putchar.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char underscore = 95;
+	char p = 112;
+	char u = 117;
+	char t = 116;
+	char c = 99;
+	char h = 104;
+	char a = 97;
+	char r = 114;
+	char newline = 10;
+
+	_putchar(underscore);
+	_putchar(p);
+	_putchar(u);
+	_putchar(t);
+	_putchar(c);
+	_putchar(h);
+	_putchar(a);
+	_putchar(r);
+	_putchar(newline);
+	return (0);
+}
