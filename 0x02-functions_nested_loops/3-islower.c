7177cfc27b3f7a175317838392a96aaca19f0250
pitsomashilo <mmapitsomashilo@gmail.com>
2023-03-21 18:00:17 +0000

    Added 3-islower.c, a C function that takes an integer as a parameter, and checks whether the letter corresponding to the integer (ASCII) is a lowercase ir uppercase letter.

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,26 @@
+ #include "main.h"
+
+/**
+ * _islower - checks whether the alphabet is lowercase.
+ * @c: an integer.
+ *
+ * The function takes a paramter of c then checks whether the parameter
+ * is lowercase or uppercase, and returns a specified value for both
+ * cases.
+ * Return: 1 if lowercase, 0 otherwise.
+ */
+_int _islower(int c)
+{
+	int result;
+
+	if (c > 96 && c < 123)
+	{
+		result = 1;
+	}
+	else
+	{
+		result = 0;
+	}
+	return (result);
+}
+
