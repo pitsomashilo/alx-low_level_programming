e3305192f0713fcfb5aa5959bd222213d807a2c2
pitsomashilo <mmapitsomashilo@gmail.com>
2023-03-22 00:25:45 +0000

    Fixed errors in 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,12 +4,13 @@
  * add - adds two integers and returns the result.
  * @a: integer
  * @b: integer
- * 
+ *
  * Return: a + b
  */
 int add(int a, int b)
 {
 	int result;
+
 	result = a + b;
 	return (result);
 }
