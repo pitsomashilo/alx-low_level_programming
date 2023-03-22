fe9be69e49028214278fa5dee403a9cd9a12037e
pitsomashilo <mmapitsomashilo@gmail.com>
2023-03-22 17:36:44 +0000

    Changed the print functions from taking integers as parameters to taking characters

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,31 +4,31 @@
  * jack_bauer - prints every minute of the day of Jack Bauer
  */
 void jack_bauer(void);
-void hours(int, int, int, int, int, int);
+void hours(char, char, char, char, char, char);
 
 void jack_bauer(void)
 {
-	int min_ones = 48;
-	int newline = 10;
-	int min_tens = 48;
-	int colon = 58;
-	int zero = 48;
-	int hour_ones = 48;
-	int hour_tens = 49;
+	int min_ones = 0;
+	int newline = '\n';
+	int min_tens = 0;
+	char colon = ':';
+	int zero = 0;
+	int hour_ones = 0;
+	int hour_tens = 1;
 
 
-	while (hour_tens < 51)
+	while (hour_tens < 3)
 	{
-		if (hour_ones == 58)
+		if (hour_ones == 10)
 		{
-			hour_ones = 48;
-			while (hour_ones < 52)
+			hour_ones = 0;
+			while (hour_ones < 4)
 			{
-				min_tens = 48;
-				while (min_tens < 54)
+				min_tens = 0;
+				while (min_tens < 6)
 				{
-					min_ones = 48;
-					while (min_ones < 58)
+					min_ones = 0;
+					while (min_ones < 10)
 					{
 						hours(hour_tens, hour_ones, colon, min_tens, min_ones, newline);
 						min_ones++;
@@ -39,15 +39,15 @@ void jack_bauer(void)
 			}
 			hour_tens++;
 		}
-		else if (hour_ones < 58)
+		else if (hour_ones <= 9)
 		{
-			while (hour_ones < 58)
+			while (hour_ones <= 9)
 			{
-				min_tens = 48;
-				while (min_tens < 54)
+				min_tens = 0;
+				while (min_tens < 6)
 				{
-					min_ones = 48;
-					while (min_ones < 58)
+					min_ones = 0;
+					while (min_ones < 10)
 					{
 						hours(zero, hour_ones, colon, min_tens, min_ones, newline);
 						min_ones++;
@@ -69,12 +69,12 @@ void jack_bauer(void)
  * @b: character
  * @c: character
  */
-void hours(int x, int y, int z, int a, int b, int c)
+void hours(char x, char y, char z, char a, char b, char c)
 {
-	_putchar(x);
-	_putchar(y);
+	_putchar(x + '0');
+	_putchar(y + '0');
 	_putchar(z);
-	_putchar(a);
-	_putchar(b);
+	_putchar(a + '0');
+	_putchar(b + '0');
 	_putchar(c);
 }
