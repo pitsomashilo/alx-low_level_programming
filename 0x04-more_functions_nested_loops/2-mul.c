bcf5f303dc4b2ed30afe0b702e0719363a34ac6c
pitsomashilo <mmapitsomashilo@gmail.com>
2023-03-23 17:53:11 +0000

    Added 2-mul.c, a C function that takes two integers as arguments, multiplies them and returns the result

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers and returns the result.
+ * @a: integer
+ * @b: integer
+ *
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
