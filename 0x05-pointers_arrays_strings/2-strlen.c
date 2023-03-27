82ac75ceaf6a7b6b0865090600d5b78fe1896606
pitsomashilo <mmapitsomashilo@gmail.com>
2023-03-27 20:00:00 +0000

    Added 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: a string
+ * Return: len
+ */
+int _strlen(char *s)
+{
+	int len;
+	int i;
+
+	i = 0;
+	len = 0;
+
+	while (s[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+	return (len);
+}
