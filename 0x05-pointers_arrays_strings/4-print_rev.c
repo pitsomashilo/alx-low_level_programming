534740bba1466a81183f72cce85f872918a1006f
pitsomashilo <mmapitsomashilo@gmail.com>
2023-03-27 20:01:53 +0000

    Added 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: a string
+ */
+void print_rev(char *s);
+int _strlen(char *s);
+
+void print_rev(char *s)
+{
+	int len;
+	int a;
+
+	len = _strlen(s);
+
+	for (a = len; a > -1; a--)
+	{
+		if (len == 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar(s[a]);
+		}
+	}
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: a string
+ * Return: len
+ */
+int _strlen(char *s)
+{
+	int len;
+	int i;
+
+	i = 0;
+	len = 0;
+
+	while (s[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+	return (len);
+}
