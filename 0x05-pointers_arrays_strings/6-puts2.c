88411edbd5248955223233b97f603dfed4f2357a
pitsomashilo <mmapitsomashilo@gmail.com>
2023-03-28 06:50:31 +0000

    Added _strlen function to 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,15 +4,20 @@
  * puts2 - prints every other character of a string, followed by a new line
  * @str: a string
  */
+void puts2(char *str);
+int _strlen(char *s);
+
 void puts2(char *str)
 {
 	int i;
+	int len;
 
-	for (i = 0;; i += 2)
+	len = _strlen(str);
+	for (i = 0; i <= len; i += 2)
 	{
-		if (str[i] == '\0')
+
+		if (str[i] == '\\')
 		{
-			_putchar('\n');
 			break;
 		}
 		else
@@ -20,4 +25,26 @@ void puts2(char *str)
 			_putchar(str[i]);
 		}
 	}
+	_putchar('\n');
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: a string
+ * Return: len
+ */
+int _strlen(char *s)
+{
+	int len;
+	int i;
+
+	i = 0;
+	len = 0;
+
+	while (s[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+	return (len);
 }
