6e4b5c751a3c5a209f949c2777f0eec97db91bb7
pitsomashilo <mmapitsomashilo@gmail.com>
2023-03-29 13:09:11 +0000

    Added 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies a string
+ * @dest: string destination
+ * @src: string to be copied
+ */
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+char *_strcpy(char *dest, char *src)
+{
+	int len;
+	int i;
+
+	len = _strlen(src) + 1;
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
+/**
+ * _strlen - returns the length of a string
+ * @s: a string
+ * Return: len
+ */
+int _strlen(char *s)
+{
+	int len;
+	int i;
+
+	i = 0;
+	len = 0;
+
+	while (s[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+	return (len);
+}
