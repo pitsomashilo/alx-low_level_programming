ea34136b450eb8b7d11e5b4f1e247c095179bf04
pitsomashilo <mmapitsomashilo@gmail.com>
2023-03-31 00:31:12 +0000

    Added 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,22 +12,17 @@ int _strcmp(char *s1, char *s2)
 	int i;
 	int result;
 
-	if (s1[0] != s2[0])
+
+	for (i = 0; s1[i] != '\0'; i++)
 	{
-		result = s1[0] - s2[0];
-	}
-	else
-	{
-		for (i = 0; s1[i]; i++)
+		if (s1[i] != s2[i])
 		{
-			if (s1[i] == s2[i])
-			{
-				result = 0;
-			}
-			else
-			{
-				result = s1[i] - s2[i];
-			}
+			result = s1[i] - s2[i];
+			break;
+		}
+		else if (s1[i] == s2[i])
+		{
+			result = (s1[i] == s2[i]) ? s1[i] - s2[i] : s1[i] - s2[i];
 		}
 	}
 	return (result);
