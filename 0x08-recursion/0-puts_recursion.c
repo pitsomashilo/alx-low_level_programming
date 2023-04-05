e21216097a865e80c44d137aa458ff6bc6ac9e7d
pitsomashilo <mmapitsomashilo@gmail.com>
2023-04-05 12:57:46 +0000

    Added the following files:
    0. 0-puts_recursion.c
    1. main.h
    2. README.md

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: a string
+ */
+void puts_recursion(char *s);
+void recurse_with_num(char *s, int n);
+
+void _puts_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+	recurse_with_num(s, i);
+}
+/**
+ * recurse_with_num - prints string recursively followed by a new line
+ * @s: string
+ * @n: integer
+ */
+void recurse_with_num(char *s, int n)
+{
+
+	_putchar(s[n]);
+	n++;
+	if (s[n] == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	recurse_with_num(s, n);
+}
