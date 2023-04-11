3573a9cb8f059f537894b9a13d9c12f998e40964
pitsomashilo <mmapitsomashilo@gmail.com>
2023-04-11 19:47:32 +0000

    Malloc return handled

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,6 +16,10 @@ char *create_array(unsigned int size, char c)
 
 	char *array = malloc(size * sizeof(char));
 
+	if (array == NULL)
+	{
+		return (NULL);
+	}
 	if (size != 0)
 	{
 		for (i = 0; i < size; i++)
