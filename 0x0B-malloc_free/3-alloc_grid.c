d02b067e036d5640620354185badf5bd2ef078cd
pitsomashilo <mmapitsomashilo@gmail.com>
2023-04-12 00:10:18 +0000

    Fixed pointer error in 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,7 +23,7 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	grid = malloc(height * sizeof(int));
+	grid = malloc(height * sizeof(int *));
 	if (grid == NULL)
 	{
 		return (NULL);
