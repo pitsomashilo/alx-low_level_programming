0d9455add77d1ac6430034211a05b3a81cb19ba5
pitsomashilo <mmapitsomashilo@gmail.com>
2023-04-13 15:18:04 +0000

    Added 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,6 +9,9 @@
  *
  * Return: void pointer
  */
+void *_calloc(unsigned int nmemb, unsigned int size);
+void *_memset(void *s, int b, unsigned int n);
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
@@ -22,5 +25,27 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (NULL);
 	}
+	ptr = _memset(ptr, 0, sizeof(ptr));
 	return (ptr);
+}
+#include "main.h"
+
+/**
+ * _memset - fills the first n bytes of the memeory area pointed to by s with
+ * constant byte b
+ * @s: address
+ * @b: constant byte
+ * @n: unsigned integer
+ *
+ * Return: a pointer to the memory area s
+ */
+void *_memset(void *s, int b, unsigned int n)
+{
+	unsigned int i; /* counter variable */
+
+	for (i = 0; i < n; i++)
+	{
+		*((int *)s + i) = b;
+	}
+	return (s);
 }
