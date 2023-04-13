f58989f397628b5ead96a5521f2a1b0b100d1734
pitsomashilo <mmapitsomashilo@gmail.com>
2023-04-13 16:56:30 +0000

    Fixed mem allocation error in 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -38,8 +38,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	len_one = _strlen(s1);
 	len_two = _strlen(s2);
 
-	len = (n >= len_two) ? len_one + len_two + 1 : len_one + n + 1;
-	new_string = malloc(len * sizeof(char));
+	len = (n >= len_two) ? len_one + len_two : len_one + n;
+	new_string = malloc(len * sizeof(char) + 1);
 	if (new_string == NULL)
 	{
 		return (NULL);
