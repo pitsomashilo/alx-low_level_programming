ed711d34d92c4bbb9e4dc05a5f1fc3a50151ccca
pitsomashilo <mmapitsomashilo@gmail.com>
2023-04-13 17:12:57 +0000

    Removed non-functional statements in 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -59,11 +59,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			new_string[len_one] = s2[j];
 			len_one++;
 		}
-		else if (len_two != 0)
-		{
-			new_string[len_one] = s2[j];
-			len_one++;
-		}
 	}
 	new_string[len] = '\0';
 	return (new_string);
