b78ad2ee60aa1b35938136d6cb32bcaf61f4771b
pitsomashilo <mmapitsomashilo@gmail.com>
2023-04-17 20:29:09 +0000

    Added the following:
    => README.md
    => dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - stores data about dogs
+ * @name: string - dog name
+ * @age: float - dog's age
+ * @owner: string - name of owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
