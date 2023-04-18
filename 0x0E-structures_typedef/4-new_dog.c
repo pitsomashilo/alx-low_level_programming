34a9b1fb39e899d04657b67f38ef88aa18bb333e
pitsomashilo <mmapitsomashilo@gmail.com>
2023-04-18 08:50:16 +0000

    Made the following changes to dog.h:
    => Defined a new type dog_t as new name for the type struct dog.
    => Added a prototype for the function new_dog
    => Added a prototype for the function free_dog
    
    Added the following files:
    => 4-new_dog.c
    => 5-free_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name: string - name of dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: NULL if function fails, new_dog otherwise
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+
+	new_dog = malloc(sizeof(struct dog));
+	if (new_dog == NULL)
+		return (NULL);
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+	return (new_dog);
+}
