a84e3c462a605c0b1a6aa6c159318db8240f685e
pitsomashilo <mmapitsomashilo@gmail.com>
2023-04-19 12:33:35 +0000

    Fixed errors

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -19,13 +19,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (array != NULL && cmp != NULL)
 		for (i = 0; i < size; i++)
 		{
-			result = (*cmp)(array[i]);
+			result = cmp(array[i]);
 			if (result != 0)
 			{
 				result = i;
 				break;
 			}
-			else
+			else if (i == size - 1 && result == 0)
 			{
 				result = -1;
 			}
