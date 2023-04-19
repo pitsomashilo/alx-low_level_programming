7bd31bb7f0a6aa41148e767ebde9dc250365a51a
pitsomashilo <mmapitsomashilo@gmail.com>
2023-04-19 12:43:36 +0000

    Made changes to 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -19,13 +19,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (array != NULL && cmp != NULL)
 		for (i = 0; i < size; i++)
 		{
-			result = cmp(array[i]);
-			if (result != 0)
+			if (cmp(array[i]) != 0)
 			{
 				result = i;
 				break;
 			}
-			else if (i == size - 1 && result == 0)
+			else
 			{
 				result = -1;
 			}
