9bf983196ca02a4c231bf6a3c3750b8c46e2513d
pitsomashilo <mmapitsomashilo@gmail.com>
2023-04-20 17:20:15 +0000

    Added conditional statement to test for empty separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -8,6 +8,9 @@
  * @separator: character that separates the numbers when printed
  * @n: number of arguments
  */
+void print_numbers(const char *separator, const unsigned int n, ...);
+int _strlen(const char *s);
+
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
@@ -21,14 +24,34 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		num = va_arg(numbers, int);
-		if (i == (n - 1) || n == 1)
+		if (i == (n - 1))
 		{
 			printf("%d\n", num);
 			break;
 		}
-		if (separator == NULL)
+		if (separator == NULL || _strlen(separator) == 0)
 			printf("%d", num);
 		else
 			printf("%d%s", num, separator);
 	}
+}
+/**
+ * _strlen - returns the length of a string
+ * @s: a string
+ * Return: len
+ */
+int _strlen(const char *s)
+{
+	int len;
+	int i;
+
+	i = 0;
+	len = 0;
+
+	while (s[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+	return (len);
 }
