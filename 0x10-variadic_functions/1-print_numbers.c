3e2c3207fba966c9cd0ab0b897ab98fca9f51d64
pitsomashilo <mmapitsomashilo@gmail.com>
2023-04-20 16:52:58 +0000

    Added conditional statement

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,7 +21,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		num = va_arg(numbers, int);
-		if (i == (n - 1))
+		if (i == (n - 1) || n == 1)
 		{
 			printf("%d\n", num);
 			break;
